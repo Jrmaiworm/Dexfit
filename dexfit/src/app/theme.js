07dcc928dd6b490fb5598810ce96cb239ef69cb0
Jorge <Jrmaiworm@hotmail.com>
2023-06-23 18:02:02 -0300

    feat: tema

---
+++ dexfit/src/app/theme.js
@@ -0,0 +1,26 @@
+
+const theme = {
+    colors: {
+      primary: '#FF0181',
+      primaryText: 'white',
+      secondary: '#EF3693',
+      secondaryText:'#8F85B0'
+      
+    },
+    fonts: {
+      name: 'Montserrat',
+      thin: 'Montserrat-Thin', // 100
+      extraLight: 'Montserrat-ExtraLight', // 200
+      light: 'Montserrat-Light', // 300
+      regular: 'Montserrat-Regular', // 400
+      medium: 'Montserrat-Medium', // 500
+      semiBold: 'Montserrat-SemiBold', // 600
+      bold: 'Montserrat-Bold', // 700
+      extraBold: 'Montserrat-ExtraBold', // 800
+      black: 'Montserrat-Black', // 900
+    },
+  };
+  
+  export default theme;
+
+  //temos que setar as fontes e as cores nao estão certas ainda
